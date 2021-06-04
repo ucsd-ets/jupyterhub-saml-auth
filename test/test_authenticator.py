f0962ef1e8ea0302aed2afbd2d9d688ed27c238e
wesuuu <wuykimpang@ucsd.edu>
2021-06-04 14:04:49 -0700

    Added firefox to github actions & cleaned up tests

--- test/test_authenticator.py
+++ test/test_authenticator.py
@@ -18,20 +18,21 @@ def driver(pytestconfig):
     if browser == 'firefox':
         options = webdriver.FirefoxOptions()
         if headless:
-            options.set_headless()
+            options.headless = True
         driver = webdriver.Firefox(options=options)
     elif browser == 'chrome':
         options = webdriver.ChromeOptions()
         if headless:
-            options.set_headless()
+            options.headless = True
         driver = webdriver.Chrome(options=options)
-    
+
     else:
         raise Exception(f'No browser option available for {browser}')
 
     yield driver
-    
+
     driver.quit()
+
 
 def test_authentication(driver):
     driver.get('http://localhost:8000/hub/saml_login')
@@ -39,7 +40,7 @@ def test_authentication(driver):
         expected_conditions.element_to_be_clickable(
             (By.ID, 'username'))
     ).send_keys('user1')
-    
+
     WebDriverWait(driver, SECONDS_WAIT).until(
         expected_conditions.element_to_be_clickable(
             (By.ID, 'password'))
@@ -49,19 +50,26 @@ def test_authentication(driver):
         expected_conditions.element_to_be_clickable(
             (By.CSS_SELECTOR, '.btn'))
     ).click()
-    
-    time.sleep(5) # allow some time for server to spawn
-    
+
+    # allow some time for server to spawn
+    time.sleep(5)
+
     assert driver.current_url == 'http://localhost:8000/user/user1/tree?'
-    
-    cookies_names_to_check = {'PHPSESSIDIDP', 'SimpleSAMLAuthTokenIdp', 'jupyterhub-session-id'}
-    current_cookies = set(map(lambda cookie: cookie['name'], driver.get_cookies()))
+
+    cookies_names_to_check = {
+        'PHPSESSIDIDP',
+        'SimpleSAMLAuthTokenIdp',
+        'jupyterhub-session-id'
+    }
+    current_cookies = set(
+        map(lambda cookie: cookie['name'], driver.get_cookies())
+    )
     assert cookies_names_to_check.issubset(current_cookies)
 
-    ### logout
+    # logout
     WebDriverWait(driver, SECONDS_WAIT).until(
         expected_conditions.element_to_be_clickable(
             (By.ID, 'logout'))
     ).click()
-    
+
     assert driver.get_cookies() == []
