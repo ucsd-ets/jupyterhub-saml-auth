fdcd1b06c3070fdb37bb486ed69b569e5a79d05d
wesuuu <wuykimpang@ucsd.edu>
2022-10-24 19:25:48 -0700

    Unit tests for redis cache

--- test/unit/test_cache.py
+++ test/unit/test_cache.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import MagicMock
 from jupyterhub_saml_auth.cache import *
+from redis.commands.json.path import Path as RedisJsonPath
 
 test_username = "user1"
 test_session_entry = SessionEntry(
@@ -51,6 +52,45 @@ class TestDisabledCache:
 
     def test_remove(self, disabled_cache):
         disabled_cache.remove(test_username)
+
+
+class TestRedisCache:
+    @pytest.fixture
+    def setup_redis_cache(self):
+        client = MagicMock
+        json = MagicMock
+        set = MagicMock()
+        get = MagicMock(return_value=vars(test_session_entry))
+        delete = MagicMock()
+
+        json.set = set
+        json.get = get
+        json.delete = delete
+        client.json = json
+
+        redis_cache = RedisCache(client, {})
+        return (redis_cache, client, json, set, get, delete)
+
+    def test_upsert(self, setup_redis_cache):
+        redis_cache, _, _, set, _, _ = setup_redis_cache
+        redis_cache.upsert(test_username, test_session_entry)
+        set.assert_called_with(
+            name=test_username,
+            path=RedisJsonPath.root_path(),
+            obj=vars(test_session_entry),
+            decode_keys=True,
+        )
+
+    def test_get(self, setup_redis_cache):
+        redis_cache, _, _, _, get, _ = setup_redis_cache
+        redis_cache.upsert(test_username, test_session_entry)
+        got = redis_cache.get(test_username)
+        assert got == test_session_entry
+
+    def test_remove(self, setup_redis_cache):
+        redis_cache, _, _, _, _, delete = setup_redis_cache
+        redis_cache.remove(test_username)
+        delete.assert_called_with(test_username, path=RedisJsonPath.root_path())
 
 
 @pytest.fixture
